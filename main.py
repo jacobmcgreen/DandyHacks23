6839f8ec067a925d0df60691ab9e06a579200375
AnFeng03 <anfeng08@gmail.com>
2023-11-03 21:12:44 -0400

    a

--- main.py
+++ main.py
@@ -1,98 +1,87 @@
-<<<<<<< HEAD
-# imports of all required libraries and UI components
 import math
 import datetime as dt
 import numpy as np
 import yfinance as yf
-from bokeh.io import curdoc 
-from bokeh.plotting import figure
-from bokeh.layouts import row,column
-from bokeh.models import TextInput, Button, DatePicker, MultiChoice
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
-#functions for api's
-#loads tickers for use in other functions
+# Functions for APIs
 def load_ticker_data(tickerA, tickerB, start_date, end_date):
     data_frame1 = yf.download(tickerA, start_date, end_date)
     data_frame2 = yf.download(tickerB, start_date, end_date)
     return data_frame1, data_frame2
 
-def plot_ticker_data(data, indicators , sync_axis = None):
+def plot_ticker_data(canvas, data, indicators):
     df = data
-    #days where the stock gained is when closing price is greater than opening price
     gain = df.Close > df.Open
-    #same logic but for loss
     loss = df.Open > df.Close
-    width = 12 * 60 * 60 * 1000
-    
-    if sync_axis is not None:
-        p = figure(x_axis_type ="datetime", tools = "pan,wheel_zoom,box_zoom,reset,save", width = 1000,
-                   x_range=sync_axis)
-    else:
-        p = figure(x_axis_type ="datetime", tools = "pan,wheel_zoom,box_zoom,reset,save", width = 1000)
+    width = 0.5  # Width of the bars for the candlestick plot
 
-    p.xaxis.major_label_orientation = math.pi/4
-    p.grid.grid_line_alpha = 0.25
+    fig = Figure(figsize=(10, 4))
+    ax = fig.add_subplot(111)
 
-    p.segment(df.index, df.High, df.index, df.Low, color = "black")
-    p.vbar(df.index[gain], width, df.Open[gain], df.Close[gain], fill_color="#00ff00", line_color = "#00ff00")
-    p.vbar(df.index[loss], width, df.Open[loss], df.Close[loss], fill_color="#ff0000", line_color = "#ff0000")
+    ax.xaxis_date()
+    ax.grid(True)
 
-    return p
+    ax.bar(df.index[gain], df.Close[gain] - df.Open[gain], width, bottom=df.Open[gain], color='green')
+    ax.bar(df.index[loss], df.Close[loss] - df.Open[loss], width, bottom=df.Open[loss], color='red')
+    ax.plot(df.index, df.Close, color='black')
 
+    # Embed the plot in the Tkinter GUI
+    canvas.figure = fig
+    canvas.draw()
 
+# Tkinter GUI components
+root = tk.Tk()
+root.title("Stock Data Visualization")
 
+stock1_label = tk.Label(root, text="Stock 1:")
+stock1_entry = tk.Entry(root)
+stock2_label = tk.Label(root, text="Stock 2:")
+stock2_entry = tk.Entry(root)
 
-def on_button_click(tickerA, tickerB, start_date, end_date, indicators):
-    #df stands for data frame
+start_date_label = tk.Label(root, text="Start Date:")
+start_date_entry = tk.Entry(root)
+end_date_label = tk.Label(root, text="End Date:")
+end_date_entry = tk.Entry(root)
+
+indicator_label = tk.Label(root, text="Indicators:")
+indicator_entry = tk.Entry(root)  # This could be a dropdown or multiple choice
+
+load_button = tk.Button(root, text="Load Data")
+
+# Matplotlib canvas
+canvas = FigureCanvasTkAgg(Figure(figsize=(10, 4)), master=root)
+
+# Layout using grid
+stock1_label.grid(row=0, column=0)
+stock1_entry.grid(row=0, column=1)
+stock2_label.grid(row=1, column=0)
+stock2_entry.grid(row=1, column=1)
+start_date_label.grid(row=2, column=0)
+start_date_entry.grid(row=2, column=1)
+end_date_label.grid(row=3, column=0)
+end_date_entry.grid(row=3, column=1)
+indicator_label.grid(row=4, column=0)
+indicator_entry.grid(row=4, column=1)
+load_button.grid(row=5, column=0, columnspan=2)
+canvas.get_tk_widget().grid(row=6, column=0, columnspan=2)
+
+# Button event handler
+def on_button_click():
+    tickerA = stock1_entry.get()
+    tickerB = stock2_entry.get()
+    start_date = start_date_entry.get()
+    end_date = end_date_entry.get()
+    indicators = indicator_entry.get()  # This will need to be parsed appropriately
+
     df1, df2 = load_ticker_data(tickerA, tickerB, start_date, end_date)
-    plot1 = plot_ticker_data(df1, indicators)
-    plot2 = plot_ticker_data(df2, indicators, sync_axis=plot1.x_range)
-    curdoc().clear()
-    curdoc().add_root(layout)
-    curdoc().add_root(row(plot1,plot2))
+    plot_ticker_data(canvas, df1, indicators)
 
+load_button.config(command=on_button_click)
 
-#variables and elements
-stock1_text = TextInput(title="Stock 1")
-stock2_text = TextInput(title="Stock 2")
-date_selector_start = DatePicker(title="Start Date",value="2022-01-01", min_date = "2000-01-01", max_date = dt.datetime.now().strftime("%Y-%m-%d"))
-#the end min date is 1 month after the start date because we want to make sure it isn't broken
-date_selector_end = DatePicker(title="End Date",value="2022-01-01", min_date = "2000-02-01", max_date = dt.datetime.now().strftime("%Y-%m-%d"))
-#this is where the user can select what they want to see
-indicator_selector = MultiChoice(options = ["100 Day SMA","30 Day SMA","Linear Regression Line"])
-
-loadButton = Button(label = "Load Data", button_type = "success")
-loadButton.onclick(lambda: on_button_click(stock1_text.value, stock2_text.value, date_selector_start.value, date_selector_end.value, indicator_selector.value))
-
-layout = column(stock1_text, stock2_text, date_selector_start, date_selector_end, indicator_selector, loadButton)
-
-curdoc().clear()
-curdoc().add_root(layout)
-
-=======
-import tkinter as tk
-
-class MainApp:
-    def __init__(self):
-        self.main_window = tk.Tk()
-        self.main_window.title("Main Page")
-
-        # Set the window size
-        window_width = 400
-        window_height = 200
-        screen_width = self.main_window.winfo_screenwidth()
-        screen_height = self.main_window.winfo_screenheight()
-        x_position = (screen_width - window_width) // 2
-        y_position = (screen_height - window_height) // 2
-        self.main_window.geometry(f"{window_width}x{window_height}+{x_position}+{y_position}")
-
-        self.label = tk.Label(self.main_window, text="Welcome to the Main Page")
-        self.label.pack()
-
-    def run(self):
-        self.main_window.mainloop()
-
-if __name__ == "__main__":
-    main_app = MainApp()
-    main_app.run()
->>>>>>> 6e624421025c078408a5e44018ad84f385ce72bb
+root.mainloop()
