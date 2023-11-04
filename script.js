ae158cf362f75fd76ba590533c2db0138fded5a2
jacobmcgreen <119445109+jacobmcgreen@users.noreply.github.com>
2023-11-04 15:42:53 -0400

    fixed all the math because we are terrible

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ document.addEventListener("DOMContentLoaded", function () {
       const age = parseFloat(document.getElementById("age").value);
       const weight = parseFloat(document.getElementById("weight").value);
       const height = parseFloat(document.getElementById("height").value);
-      const experience = document.getElementById("fitness-level").value;
+      const fitnesslevel = document.getElementById("fitness-level").value;
 
       // Calculate data
-      const resultValue = Math.floor(calculateCalorieIntake(gender, weight, height, age, fitnessGoals, experience));
+      const resultValue = Math.floor(calculateCalorieIntake(gender, weight, height, age, fitnessGoals, fitnesslevel));
       
       // Display the result in an HTML element
       const resultElement = document.getElementById("result");
@@ -24,21 +24,29 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-function calculateCalorieIntake(gender, weight, height, age, fitnessGoals, experience) {
+function calculateCalorieIntake(gender, weight, height, age, fitnessGoals, fitnesslevel) {
   let BMR = 0;
   if (gender === "Male") {
-      BMR = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
+      BMR = ((10*(weight*0.453592))+(6.25*(height*2.54))-(5*age)+5);
   } else if (gender === "Female") {
-      BMR = 655 + (4.3 * weight) + (4.7 * height) - (4.7 * age);
+      BMR = ((10*(weight*0.453592))+(6.25*(height*2.54))-(5*age)-161);
   }
   
   let TDEE = 0;
-  if (experience === "beginner") { 
+  if (fitnesslevel === "BMR") { 
+      TDEE = 1 * BMR;
+  } else if (fitnesslevel === "SED") {
+      TDEE = 1.2 * BMR;
+  } else if (fitnesslevel === "LIG") {
       TDEE = 1.375 * BMR;
-  } else if (experience === "intermediate") {
-      TDEE = 1.55 * BMR;
-  } else if (experience === "advanced") {
-      TDEE = 1.725 * BMR;
+  } else if (fitnesslevel === "MOD") {
+    TDEE = 1.465 * BMR;
+  } else if (fitnesslevel === "ACT") {
+    TDEE = 1.55 * BMR;
+  } else if (fitnesslevel === "VAA") {
+    TDEE = 1.725 * BMR;
+  }else if (fitnesslevel === "EAA") {
+    TDEE = 1.9 * BMR;
   }
   
   let maintenanceCalories = 0;
